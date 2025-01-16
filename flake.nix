30c8f567dd16364be743b2788c728857b30e869d
Palani Johnson <palanijohnson@gmail.com>
2025-01-16 13:33:02 -0700

    updated flake

--- flake.nix
+++ flake.nix
@@ -1,87 +1,36 @@
 {
-  description = "A Typst project";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    typix = {
-      url = "github:loqusion/typix";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
     };
-
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs @ {
+  outputs = {
     nixpkgs,
-    typix,
     flake-utils,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      inherit (pkgs) lib;
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "typst-cv";
+          packages = with pkgs; [
+            # Typst
+            typst
+            typstfmt
 
-      typixLib = typix.lib.${system};
-
-      src = typixLib.cleanTypstSource ./modern-cv;
-
-      commonArgs = {
-        typstSource = "./modern-cv/resume.typ";
-        typstOutput = "./out/resume.pdf";
-
-        fontPaths = [
-          pkgs.roboto
-          pkgs.source-sans
-          pkgs.source-sans-pro
-          pkgs.font-awesome
-        ];
-      };
-
-      # Compile a Typst project, *without* copying the result
-      # to the current directory
-      build-drv = typixLib.buildTypstProject (commonArgs
-        // {
-          inherit src;
-        });
-
-      # Compile a Typst project, and then copy the result
-      # to the current directory
-      build-script = typixLib.buildTypstProjectLocal (commonArgs
-        // {
-          inherit src;
-        });
-
-      # Watch a project and recompile on changes
-      watch-script = typixLib.watchTypstProject commonArgs;
-    in {
-      checks = {
-        inherit build-drv build-script watch-script;
-      };
-
-      packages.default = build-drv;
-
-      apps = rec {
-        default = watch;
-        build = flake-utils.lib.mkApp {
-          drv = build-script;
+            # Fonts
+            roboto
+            source-sans
+            source-sans-pro
+            font-awesome
+          ];
         };
-        watch = flake-utils.lib.mkApp {
-          drv = watch-script;
-        };
-      };
-
-      devShells.default = typixLib.devShell {
-        inherit (commonArgs) fontPaths;
-
-        name = "resume";
-        packages = [
-          # WARNING: Don't run `typst-build` directly, instead use `nix run .#build`
-          # See https://github.com/loqusion/typix/issues/2
-          # build-script
-          watch-script
-          pkgs.typstfmt
-        ];
-      };
-    });
+      }
+    );
 }
